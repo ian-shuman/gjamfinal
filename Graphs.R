0e18ca811ff22ed5625036d95fa48f25e9b7a63c
Alyssa Willson <awillso2@nd.edu>
2023-03-02 13:45:06 -0500

    allowing multiple chains

--- Graphs.R
+++ Graphs.R
@@ -4,7 +4,11 @@ library(tidyverse)
 library(corrplot)
 library(cowplot)
 
-load('out/reduced_taxa-all_cov+latlong.RData')
+#load('out/reduced_taxa-all_cov+latlong.RData')
+load('out/reduced_taxa-temp+precip_1.RData')
+out1 <- out
+load('out/reduced_taxa-temp+precip_2.RData')
+out2 <- out
 
 ### Trace plots
 
@@ -15,69 +19,99 @@ load('out/reduced_taxa-all_cov+latlong.RData')
 ## fSensGibbs: sensitivity (fMu)
 ## sgibbs: covariance (sigMu)
 
-bFacGibbs <- out$chains$bFacGibbs
-bgibbs <- out$chains$bgibbs
-bgibbsUn <- out$chains$bgibbsUn
-fSensGibbs <- out$chains$fSensGibbs
-sgibbs <- out$chains$sgibbs
-
+bFacGibbs <- out1$chains$bFacGibbs
 bFacGibbs <- as.data.frame(bFacGibbs)
+bFacGibbs$chain <- rep(1, times = nrow(bFacGibbs))
+bFacGibbs$iter <- rownames(bFacGibbs)
+bFacGibbs2 <- out2$chains$bFacGibbs
+bFacGibbs2 <- as.data.frame(bFacGibbs2)
+bFacGibbs2$chain <- rep(2, times = nrow(bFacGibbs2))
+bFacGibbs2$iter <- rownames(bFacGibbs2)
+bFacGibbs <- rbind(bFacGibbs, bFacGibbs2)
+
+bgibbs <- out1$chains$bgibbs
+bgibbs <- as.data.frame(bgibbs)
+bgibbs$chain <- rep(1, times = nrow(bgibbs))
+bgibbs$iter <- rownames(bgibbs)
+bgibbs2 <- out2$chains$bgibbs
+bgibbs2 <- as.data.frame(bgibbs2)
+bgibbs2$chain <- rep(2, times = nrow(bgibbs2))
+bgibbs2$iter <- rownames(bgibbs2)
+bgibbs <- rbind(bgibbs, bgibbs2)
+
+bgibbsUn <- out1$chains$bgibbsUn
+bgibbsUn <- as.data.frame(bgibbsUn)
+bgibbsUn$chain <- rep(1, times = nrow(bgibbsUn))
+bgibbsUn$iter <- rownames(bgibbsUn)
+bgibbsUn2 <- out2$chains$bgibbsUn
+bgibbsUn2 <- as.data.frame(bgibbsUn2)
+bgibbsUn2$chain <- rep(2, times = nrow(bgibbsUn2))
+bgibbsUn2$iter <- rownames(bgibbsUn2)
+bgibbsUn <- rbind(bgibbsUn, bgibbsUn2)
+
+fSensGibbs <- out1$chains$fSensGibbs
+fSensGibbs <- as.data.frame(fSensGibbs)
+fSensGibbs$chain <- rep(1, times = nrow(fSensGibbs))
+fSensGibbs$iter <- rownames(fSensGibbs)
+fSensGibbs2 <- out2$chains$fSensGibbs
+fSensGibbs2 <- as.data.frame(fSensGibbs2)
+fSensGibbs2$chain <- rep(2, times = nrow(fSensGibbs2))
+fSensGibbs2$iter <- rownames(fSensGibbs2)
+fSensGibbs <- rbind(fSensGibbs, fSensGibbs2)
+
+sgibbs <- out1$chains$sgibbs
+sgibbs <- as.data.frame(sgibbs)
+sgibbs$chain <- rep(1, times = nrow(sgibbs))
+sgibbs$iter <- rownames(sgibbs)
+sgibbs2 <- out2$chains$sgibbs
+sgibbs2 <- as.data.frame(sgibbs2)
+sgibbs2$chain <- rep(2, times = nrow(sgibbs2))
+sgibbs2$iter <- rownames(sgibbs2)
+sgibbs <- rbind(sgibbs, sgibbs2)
 
 bFacGibbs |>
-  rownames_to_column() |>
-  mutate(rowname = as.numeric(rowname)) |>
+  mutate(iter = as.numeric(iter)) |>
   pivot_longer(cols = Prairie_totalPPT:Savanna_MeanTEMP, names_to = "beta", values_to = "estimate") |>
-  filter(rowname > 200) |>
-  ggplot(aes(x = rowname, y = estimate)) +
+  filter(iter > 200) |>
+  ggplot(aes(x = iter, y = estimate, color = as.factor(chain))) +
   geom_line() +
   facet_wrap(~beta, scales = 'free')
-
-bgibbs <- as.data.frame(bgibbs)
 
 bgibbs |>
-  rownames_to_column() |>
-  mutate(rowname = as.numeric(rowname)) |>
+  mutate(iter = as.numeric(iter)) |>
   pivot_longer(cols = Prairie_intercept:Savanna_MeanTEMP, names_to = "beta", values_to = "estimate") |>
-  filter(rowname > 200) |>
-  ggplot(aes(x = rowname, y = estimate)) +
+  filter(iter > 200) |>
+  ggplot(aes(x = iter, y = estimate, color = as.factor(chain))) +
   geom_line() +
   facet_wrap(~beta, scales = 'free')
-
-bgibbsUn <- as.data.frame(bgibbsUn)
 
 bgibbsUn |>
-  rownames_to_column() |>
-  mutate(rowname = as.numeric(rowname)) |>
+  mutate(iter = as.numeric(iter)) |>
   pivot_longer(cols = Prairie_intercept:Savanna_MeanTEMP, names_to = "beta", values_to = "estimate") |>
-  filter(rowname > 200) |>
-  ggplot(aes(x = rowname, y = estimate)) +
+  filter(iter > 200) |>
+  ggplot(aes(x = iter, y = estimate, color = as.factor(chain))) +
   geom_line() +
   facet_wrap(~beta, scales = 'free')
 
-fSensGibbs <- as.data.frame(fSensGibbs)
-
 fSensGibbs |>
-  rownames_to_column() |>
-  mutate(rowname = as.numeric(rowname)) |>
+  mutate(iter = as.numeric(iter)) |>
   pivot_longer(cols = totalPPT:MeanTEMP, names_to = 'f', values_to = 'estimate') |>
-  filter(rowname > 200) |>
-  ggplot(aes(x = rowname, y = estimate)) +
+  filter(iter > 200) |>
+  ggplot(aes(x = iter, y = estimate, color = as.factor(chain))) +
   geom_line() +
   facet_wrap(~f, scales = 'free')
 
-sgibbs <- as.data.frame(sgibbs)
-
 sgibbs |>
-  rownames_to_column() |>
-  mutate(rowname = as.numeric(rowname)) |>
+  mutate(iter = as.numeric(iter)) |>
   pivot_longer(cols = Prairie_Prairie:Savanna_Savanna, names_to = 'sigma', values_to = 'estimate') |>
-  filter(rowname > 200) |>
-  ggplot(aes(x = rowname, y = estimate)) +
+  filter(iter > 200) |>
+  ggplot(aes(x = iter, y = estimate, color = as.factor(chain))) +
   geom_line() +
   facet_wrap(~sigma, scales = 'free')
 
 ### Let's take a look at correlations
 mean_cov <- sgibbs |>
+  select(-(chain:iter)) |>
   summarize_all(mean)
 
 mean_cov <- as.matrix(mean_cov)
@@ -92,6 +126,7 @@ rownames(mean_cov) <- c('Prairie', 'Forest', 'Savanna')
 mean_cor <- cov2cor(mean_cov)
 
 sd_cov <- sgibbs |>
+  select(-(chain:iter)) |>
   summarize_all(sd)
 
 sd_cov <- as.matrix(sd_cov)
@@ -104,8 +139,10 @@ colnames(sd_cov) <- c('Prairie', 'Forest', 'Savanna')
 rownames(sd_cov) <- c('Prairie', 'Forest', 'Savanna')
 
 lower_mat <- sgibbs |>
+  select(-(chain:iter)) |>
   summarize_all(funs(quantile(., probs = 0.05)))
 upper_mat <- sgibbs |>
+  select(-(chain:iter)) |>
   summarize_all(funs(quantile(., probs = 0.95)))
 
 lower_mat <- as.matrix(lower_mat)
@@ -124,12 +161,12 @@ upper_mat <- cov2cor(upper_mat)
 corrplot(mean_cor, lowCI.mat = lower_mat, uppCI.mat = upper_mat, diag = T, plotCI = 'circ')
 
 ### Correlations between taxa and drivers
-Prairie_totalPPT <- as.data.frame(bFacGibbs$Prairie_totalPPT)
-Prairie_MeanTEMP <- as.data.frame(bFacGibbs$Prairie_MeanTEMP)
-Forest_totalPPT <- as.data.frame(bFacGibbs$Forest_totalPPT)
-Forest_MeanTEMP <- as.data.frame(bFacGibbs$Forest_MeanTEMP)
-Savanna_totalPPT <- as.data.frame(bFacGibbs$Savanna_totalPPT)
-Savanna_MeanTEMP <- as.data.frame(bFacGibbs$Savanna_MeanTEMP)
+Prairie_totalPPT <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Prairie_totalPPT))
+Prairie_MeanTEMP <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Prairie_MeanTEMP))
+Forest_totalPPT <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Forest_totalPPT))
+Forest_MeanTEMP <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Forest_MeanTEMP))
+Savanna_totalPPT <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Savanna_totalPPT))
+Savanna_MeanTEMP <- as.data.frame(bFacGibbs |> filter(iter > 250) |> select(Savanna_MeanTEMP))
 
 betas <- matrix(, nrow = 4, ncol = 6)
 
@@ -137,63 +174,45 @@ colnames(betas) <- c('PrairiePPT', 'PrairieT', 'ForestPPT', 'ForestT', 'SavannaP
 rownames(betas) <- c('Mean', 'SD', 'Lower', 'Upper')
 
 temp <- Prairie_totalPPT |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Prairie_totalPPT`),
-            sd = sd(`bFacGibbs$Prairie_totalPPT`),
-            lower = quantile(`bFacGibbs$Prairie_totalPPT`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Prairie_totalPPT`, probs = 0.95))
+  summarize(mean = mean(Prairie_totalPPT),
+            sd = sd(Prairie_totalPPT),
+            lower = quantile(Prairie_totalPPT, probs = 0.05),
+            upper = quantile(Prairie_totalPPT, probs = 0.95))
 betas[1:4,1] <- as.vector(as.matrix(temp))
 
 temp <- Prairie_MeanTEMP |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Prairie_MeanTEMP`),
-            sd = sd(`bFacGibbs$Prairie_MeanTEMP`),
-            lower = quantile(`bFacGibbs$Prairie_MeanTEMP`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Prairie_MeanTEMP`, probs = 0.95))
+  summarize(mean = mean(Prairie_MeanTEMP),
+            sd = sd(Prairie_MeanTEMP),
+            lower = quantile(Prairie_MeanTEMP, probs = 0.05),
+            upper = quantile(Prairie_MeanTEMP, probs = 0.95))
 betas[1:4,2] <- as.vector(as.matrix(temp))
 
 temp <- Forest_totalPPT |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Forest_totalPPT`),
-            sd = sd(`bFacGibbs$Forest_totalPPT`),
-            lower = quantile(`bFacGibbs$Forest_totalPPT`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Forest_totalPPT`, probs = 0.95))
+  summarize(mean = mean(Forest_totalPPT),
+            sd = sd(Forest_totalPPT),
+            lower = quantile(Forest_totalPPT, probs = 0.05),
+            upper = quantile(Forest_totalPPT, probs = 0.95))
 betas[1:4,3] <- as.vector(as.matrix(temp))
 
 temp <- Forest_MeanTEMP |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Forest_MeanTEMP`),
-            sd = sd(`bFacGibbs$Forest_MeanTEMP`),
-            lower = quantile(`bFacGibbs$Forest_MeanTEMP`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Forest_MeanTEMP`, probs = 0.95))
+  summarize(mean = mean(Forest_MeanTEMP),
+            sd = sd(Forest_MeanTEMP),
+            lower = quantile(Forest_MeanTEMP, probs = 0.05),
+            upper = quantile(Forest_MeanTEMP, probs = 0.95))
 betas[1:4,4] <- as.vector(as.matrix(temp))
 
 temp <- Savanna_totalPPT |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Savanna_totalPPT`),
-            sd = sd(`bFacGibbs$Savanna_totalPPT`),
-            lower = quantile(`bFacGibbs$Savanna_totalPPT`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Savanna_totalPPT`, probs = 0.95))
+  summarize(mean = mean(Savanna_totalPPT),
+            sd = sd(Savanna_totalPPT),
+            lower = quantile(Savanna_totalPPT, probs = 0.05),
+            upper = quantile(Savanna_totalPPT, probs = 0.95))
 betas[1:4,5] <- as.vector(as.matrix(temp))
 
 temp <- Savanna_MeanTEMP |>
-  rownames_to_column() |>
-  filter(rowname > 250) |>
-  column_to_rownames(var = 'rowname') |>
-  summarize(mean = mean(`bFacGibbs$Savanna_MeanTEMP`),
-            sd = sd(`bFacGibbs$Savanna_MeanTEMP`),
-            lower = quantile(`bFacGibbs$Savanna_MeanTEMP`, probs = 0.05),
-            upper = quantile(`bFacGibbs$Savanna_MeanTEMP`, probs = 0.95))
+  summarize(mean = mean(Savanna_MeanTEMP),
+            sd = sd(Savanna_MeanTEMP),
+            lower = quantile(Savanna_MeanTEMP, probs = 0.05),
+            upper = quantile(Savanna_MeanTEMP, probs = 0.95))
 betas[1:4,6] <- as.vector(as.matrix(temp))
 
 p1 <- ggplot() +
