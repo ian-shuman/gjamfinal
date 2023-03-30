19cd2214bc98f75b160e66dcba202d6b1a66324c
Alyssa Willson <awillso2@nd.edu>
2023-03-30 13:49:41 -0400

    Out of sample validation

--- R/5.Validate.R
+++ R/5.Validate.R
@@ -1,10 +1,13 @@
-## Validation
+## Validation for our all taxa, all cov model
+## This is separate from validation for the reduced taxa, all cov model
 
 ## Author: AM Willson
 
 rm(list = ls())
 
 library(gjam)
+library(lme4)
+library(piecewiseSEM)
 
 # Load output from GJAM
 load('out/all_taxa-all_cov/all_taxa-all_cov_1.RData')
@@ -27,6 +30,8 @@ load('GJAM DATA/Withheld For Validation/validation_process2.RData')
 nas <- length(which(is.na(xdata[,4])))
 rands <- runif(n = nas, min = min(xdata[,4], na.rm = T), max = max(xdata[,4], na.rm = T))
 xdata[is.na(xdata)] <- rands
+
+#### Simple validation ####
 
 # Specify data list
 newdata <- list(xdata = xdata, nsim = 1000, ematrix = edata)
@@ -97,3 +102,137 @@ comp |>
   geom_point() +
   xlim(c(0, 1)) + ylim(c(0, 1)) +
   facet_wrap(~Taxon)
+
+pred_pr <- pred_pr |>
+  pivot_longer(No.tree:Other.hardwood, names_to = 'Taxon', values_to = 'Probability') |>
+  mutate(Index = ydata$Index)
+
+comp <- pred_pr |>
+  full_join(ydata, by = c('Index', 'Taxon'))
+colnames(comp) <- c('Taxon', 'Probability', 'Index', 'Observed')
+
+comp |>
+  ggplot(aes(x = Probability, y = Observed)) +
+  geom_point() +
+  facet_wrap(~Taxon, scales = 'free') +
+  geom_smooth(method = 'glm', method.args = list(family = 'binomial'))
+
+# Overall model
+form <- glm(Observed ~ Probability, family = binomial, data = comp)
+summary(form)
+# Overall R2
+with(summary(form), 1 - deviance/null.deviance)
+
+# Overall with random effect
+comp$Taxon <- as.factor(comp$Taxon)
+form_rand <- glmer(Observed ~ Probability + (1|Taxon), family = binomial, data = comp)
+summary(form_rand)
+piecewiseSEM::rsquared(form_rand)
+
+# Individual models
+comp_notree <- comp |>
+  filter(Taxon == 'No.tree')
+comp_oak <- comp |>
+  filter(Taxon == 'Oak')
+comp_elm <- comp |>
+  filter(Taxon == 'Elm')
+comp_hickory <- comp |>
+  filter(Taxon == 'Hickory')
+comp_ash <- comp |>
+  filter(Taxon == 'Ash')
+comp_maple <- comp |>
+  filter(Taxon == 'Maple')
+comp_basswood <- comp |>
+  filter(Taxon == 'Basswood')
+comp_walnut <- comp |>
+  filter(Taxon == 'Walnut')
+comp_ironwood <- comp |>
+  filter(Taxon == 'Ironwood')
+comp_beech <- comp |>
+  filter(Taxon == 'Beech')
+comp_dogwood <- comp |>
+  filter(Taxon == 'Dogwood')
+comp_poplar <- comp |>
+  filter(Taxon == 'Poplar.tulip.poplar')
+comp_gum <- comp |>
+  filter(Taxon == 'Black.gum.sweet.gum')
+comp_otherconifer <- comp |>
+  filter(Taxon == 'Other.conifer')
+comp_otherhardwood <- comp |>
+  filter(Taxon == 'Other.hardwood')
+
+form_notree <- glm(Observed ~ Probability, family = binomial, data = comp_notree)
+with(summary(form_notree), 1 - deviance/null.deviance)
+
+form_oak <- glm(Observed~Probability, family = binomial, data = comp_oak)
+with(summary(form_oak), 1 - deviance/null.deviance)
+
+form_elm <- glm(Observed ~ Probability, family = binomial, data = comp_elm)
+with(summary(form_elm), 1 - deviance/null.deviance)
+
+form_hickory <- glm(Observed ~ Probability, family = binomial, data = comp_hickory)
+with(summary(form_hickory), 1 - deviance/null.deviance)
+
+form_ash <- glm(Observed ~ Probability, family = binomial, data = comp_ash)
+with(summary(form_ash), 1 - deviance/null.deviance)
+
+form_maple <- glm(Observed ~ Probability, family = binomial, data = comp_maple)
+with(summary(form_maple), 1 - deviance/null.deviance)
+
+form_basswood <- glm(Observed ~ Probability, family = binomial, data = comp_basswood)
+with(summary(form_basswood), 1 - deviance/null.deviance)
+
+#### Validation with conditional prediction ####
+
+load('out/all_taxa-all_cov/all_taxa-all_cov_1.RData')
+rm(edata, elist, mlist, xdata, ydata, site_effort)
+load('GJAM DATA/Withheld For Validation/validation_process2.RData')
+nas <- length(which(is.na(xdata[,4])))
+rands <- runif(n = nas, min = min(xdata[,4], na.rm = T), max = max(xdata[,4], na.rm = T))
+xdata[is.na(xdata)] <- rands
+
+# Predict half of species conditional on other half
+rand <- sample(1:ncol(ydata), size = round(ncol(ydata)/2))
+newdata <- list(xdata = xdata,
+                ydataCond = ydata[,rand],
+                nsim = 1000)
+pred2 <- gjamPredict(output = out, newdata = newdata)
+
+# Collect prediction outputs
+pred2_yMu <- as.data.frame(pred2$sdList$yMu)
+pred2_pr <- as.data.frame(pred2$prPresent)
+
+# Compare with observations
+pred2_yMu <- pred2_yMu |>
+  mutate(Index = rownames(ydata)) |>
+  pivot_longer(No.tree:Other.hardwood, names_to = 'Taxon', values_to = 'Presence')
+ydata2 <- ydata |>
+  rownames_to_column(var = 'Index') |>
+  pivot_longer(No.tree:Other.hardwood, names_to = 'Taxon', values_to = 'Presence')
+
+comp <- pred_yMu |>
+  full_join(ydata2, by = c('Index', 'Taxon'))
+colnames(comp) <- c('Index', 'Taxon', 'Predicted', 'Observed')
+
+comp |>
+  ggplot(aes(x = Predicted, y = Observed)) +
+  geom_point() +
+  facet_wrap(~Taxon)
+
+# Overall model
+form2 <- glm(Observed ~ Predicted, family = binomial, data = comp)
+summary(form2)
+# Overall R2
+with(summary(form2), 1 - deviance/null.deviance)
+# Increase in R2 (marginal)
+print(paste0('No conditional prediction: ', with(summary(form), 1 - deviance/null.deviance)))
+print(paste0('With conditional prediction: ', with(summary(form2), 1 - deviance/null.deviance)))
+
+# Overall with random effect
+comp$Taxon <- as.factor(comp$Taxon)
+form2_rand <- glmer(Observed ~ Predicted + (1|Taxon), family = binomial, data = comp)
+summary(form2_rand)
+piecewiseSEM::rsquared(form2_rand)
+# Marginal decrease in R2
+print(paste0('No conditional prediction: ', piecewiseSEM::rsquared(form_rand)$Conditional))
+print(paste0('With conditional prediction: ', piecewiseSEM::rsquared(form2_rand)$Conditional))
