13855c61b61c0682dd06fb40fd08c87b7f7f4054
Alyssa Willson <awillso2@nd.edu>
2023-09-25 15:28:21 -0400

    Fixing and commenting Step 3

--- R/3.Reduce.R
+++ R/3.Reduce.R
@@ -1,53 +1,34 @@
 ## Reducing ydata to improve computational time
 
-## This script is really awkwardly placed betwen steps 1 & 2 right
-## now. This should be updated prior to publication
-
 ## Author: AM Willson
 
 rm(list = ls())
-library(tidyverse)
-library(corrplot)
+
+library(tidyr)
+library(dplyr)
+library(ggplot2)
 
 load('GJAMDATA/process_FINALSOILS.RData')
-load('GJAMDATA/effort_fixed.RData')
 
 # Make into a more usable format
 longydata <- ydata |>
-  pivot_longer(cols = c(No.tree:Chestnut))
-colnames(longydata) <- c('Taxon', 'PA')
+  pivot_longer(cols = c(No.tree:Chestnut),
+              names_to = 'Taxon', values_to = 'PA')
 
-longeffort <- effort |>
-  pivot_longer(cols = c(No.tree_dist:Chestnut_dist))
-colnames(longeffort) <- c('Taxon', 'Effort')
-
-# How many presences at a corner does each taxon have?
+# How many presences does each taxon have across all corners?
 counts <- longydata |>
   group_by(Taxon) |>
   summarize(count = length(which(PA == 1))) |>
   arrange(desc(count))
+print(counts, n = 32)
 
-# There are < 10 observations of alder, tulip poplar, cedar juniper,
+# There are < 10 observations of alder, tulip poplar, cedar/juniper,
 # and chestnut, so those shouldn't be categories
 # Let's remove alder, cedar juniper, and chestnut
 longydata <- longydata |>
   filter(Taxon != 'Alder',
          Taxon != 'Cedar.juniper',
          Taxon != 'Chestnut')
-
-# Make sure it looks correct
-test <- longydata |>
-  group_by(Taxon) |>
-  summarize(count = length(which(PA == 1))) |>
-  arrange(desc(count))
-print(test, n = nrow(test))
-
-# Since we have a category of "tulip poplar/poplar" that's much bigger,
-# let's combine the "tulip poplar" "poplar" and "tulip poplar/poplar"
-# categories
-#longydata <- longydata |>
-#  mutate(Taxon = replace(Taxon, Taxon == 'Tulip.poplar', 'Poplar.tulip.poplar'),
-#         Taxon = replace(Taxon, Taxon == 'Poplar', 'Poplar.tulip.poplar'))
 
 # Make sure it looks correct
 test <- longydata |>
@@ -74,15 +55,13 @@ p1 <- longydata |>
   ylab('Number of Observations') +
   xlab('') +
   theme_minimal()
+p1
 
 ## Based on this, I would definitely say that there is a 
 ## reasonable cut-off around "Walnut" where we could group all other
 ## species together
 
-## Alternatively, species below "Walnut" could be grouped with a
-## functionally similar species above "Walnut" where applicable
-
-## For now, I will group everything from Bald cypress:Black.gum in
+## I will group everything from Bald cypress:Black.gum in
 ## either "Other hardwood" or "Other conifer"
 
 longydata <- longydata |>
@@ -96,21 +75,11 @@ longydata <- longydata |>
          Taxon = replace(Taxon, Taxon == 'Sycamore', 'Other.hardwood'),
          Taxon = replace(Taxon, Taxon == 'Buckeye', 'Other.hardwood'),
          Taxon = replace(Taxon, Taxon == 'Hackberry', 'Other.hardwood'),
-         Taxon = replace(Taxon, Taxon == 'Mulberry', 'Other.hardwood'))
-## Do the same for the effort
-
-longeffort <- longeffort |>
-  mutate(Taxon = replace(Taxon, Taxon == 'Bald.cypress_dist', 'Other.conifer_dist'),
-         Taxon = replace(Taxon, Taxon == 'Pine_dist', 'Other.conifer_dist'),
-         Taxon = replace(Taxon, Taxon == 'Birch_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Tamarack_dist', 'Other.conifer_dist'),
-         Taxon = replace(Taxon, Taxon == 'Locust_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Willow_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Cherry_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Sycamore_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Buckeye_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Hackberry_dist', 'Other.hardwood_dist'),
-         Taxon = replace(Taxon, Taxon == 'Mulberry_dist', 'Other.hardwood_dist'))
+         Taxon = replace(Taxon, Taxon == 'Mulberry', 'Other.hardwood'),
+         Taxon = replace(Taxon, Taxon == 'Black.gum', 'Black.gum.sweet.gum'),
+         Taxon = replace(Taxon, Taxon == 'Poplar', 'Poplar.tulip.poplar'),
+         Taxon = replace(Taxon, Taxon == 'Sweet.gum', 'Black.gum.sweet.gum'),
+         Taxon = replace(Taxon, Taxon == 'Tulip.poplar', 'Poplar.tulip.poplar'))
 
 # Let's visualize again
 p2 <- longydata |>
@@ -121,6 +90,7 @@ p2 <- longydata |>
   coord_flip() +
   theme_minimal() +
   ylab('Number of Observations') + xlab('')
+p2
 
 ## This seems a lot more reasonable now. Let's insert this information
 ## back into our original "ydata" data object
@@ -168,52 +138,17 @@ p3 <- new.ydata |>
   coord_flip() +
   theme_minimal() +
   xlab('') + ylab('Number of Observations')
-
-# Redo for effort
-## This is done in two steps because we have to use the rowwise function to take means
-new.edata <- effort |>
-  rowwise() |>
-  mutate(TP_dist = mean(c(Poplar_dist, Poplar.tulip.poplar_dist, Tulip.poplar_dist), na.rm = T),
-         BGSG_dist = mean(c(Black.gum.sweet.gum_dist, Black.gum_dist, Sweet.gum_dist), na.rm = T),
-         Other.conifer_dist = mean(c(Bald.cypress_dist, Pine_dist, Tamarack_dist, Cedar.juniper_dist), na.rm = T),
-         Other.hardwood.2_dist = mean(c(Birch_dist, Locust_dist, Willow_dist, Cherry_dist,
-                                        Sycamore_dist, Buckeye_dist, Hackberry_dist, Mulberry_dist, 
-                                        Alder_dist, Chestnut_dist), na.rm = T))
-
-new.edata <- new.edata |>
-  mutate_all(~ifelse(is.nan(.), NA, .)) |>
-  select(-c(Poplar_dist, Poplar.tulip.poplar_dist, Tulip.poplar_dist,
-            Black.gum.sweet.gum_dist, Black.gum_dist, Sweet.gum_dist,
-            Bald.cypress_dist, Pine_dist, Tamarack_dist, Cedar.juniper_dist,
-            Other.hardwood_dist, Birch_dist, Locust_dist, Willow_dist, Cherry_dist, Sycamore_dist,
-            Buckeye_dist, Hackberry_dist, Mulberry_dist, Alder_dist, Chestnut_dist)) |>
-  rename(Poplar.tulip.poplar_dist = TP_dist,
-         Black.gum.sweet.gum_dist = BGSG_dist,
-         Other.hardwood_dist = Other.hardwood.2_dist)
+p3
 
 # Now, we need to make sure that we didn't create any more rows with
 # only zeros
-
 zeros <- apply(new.ydata, 1, sum)
 zeros <- which(zeros == 0)
-
-zeros <- apply(new.edata, 1, sum)
-zeros <- which(zeros == 0)
-
-# Let's make sure it looks like we think it does
-new.ydata |>
-  pivot_longer(cols = c(No.tree:Other.hardwood)) |>
-  group_by(name) |>
-  summarize(count = length(which(value == 1))) |>
-  ggplot(aes(x = reorder(name, count), y = count)) +
-  geom_point() +
-  coord_flip()
 
 # Looks like we're good!
 
 ydata <- new.ydata
-edata <- new.edata
 
 # I'm going to save this in a different object for now
-save(xdata, ydata, edata, site_effort, file = 'GJAMDATA/process2_FINALSOILS.RData')
+save(xdata, ydata, file = 'GJAMDATA/process2_FINALSOILS.RData')
 
