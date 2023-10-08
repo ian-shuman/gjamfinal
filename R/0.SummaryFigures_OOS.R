bfb2a1237e1e887d0d73a7b18e485474e224d76b
Alyssa Willson <awillso2@nd.edu>
2023-10-07 21:53:34 -0400

    Adding more summary figures and improving figure quality

--- R/0.SummaryFigures_OOS.R
+++ R/0.SummaryFigures_OOS.R
@@ -1,7 +1,7 @@
-## This script provides visualizations of all variables used in the analysis
-## which are included in the supplementary information of Shuman et al. (in prep)
+## Script for visualizing out-of-sample regions
+## These plots can be compared with the prediction derived from GJAM
 
-## Authors: I Shuman & AM Willson
+## Author: AM Willson & I Shuman
 
 rm(list = ls())
 
@@ -16,41 +16,40 @@ library(tidyr)
 library(dplyr)
 library(tibble)
 
-# Map of study region for plotting
+# Map of the study region for plotting
 states <- map_data('state') |>
   dplyr::filter(region %in% c('indiana', 'illinois'))
 
-### Summary Figures for Y Data
+### Summary Figures for out-of-sample Y data
 
-# Load taxon-level ydata
-load('GJAMDATA/processed_xydata_2.RData')
+# Load taxon-level yata
+load('GJAMDATA/Withheld For Validation/validation_processed_xydata_fixmarea_reduced.RData')
 # Rename
-ydata_all <- ydata
+ydata_all <- ydata_oos
 # Load ecosystem-level ydata & xdata
-load('GJAMDATA/processed_xydata_2_ecosystem.RData')
+load('GJAMDATA/Withheld For Validation/validation_processed_xydata_fixmarea_reduced_ecosystem.RData')
 # Rename
-ydata_eco <- ydata
+ydata_eco <- ydata_oos
 
 # Add corner ID to xdata
-xdata_ind <- xdata |>
+xdata_ind <- xdata_oos |>
   rownames_to_column(var = 'id')
 
 # Add lat/long to taxon-level data
 ydata_all_comb <- ydata_all |>
   rownames_to_column(var = 'id') |>
   left_join(xdata_ind, by = 'id') |>
-  select(c(No.tree:Other.hardwood, lat, long))
+  select(c(Elm:Other.hardwood, lat, long))
 
-# Add lat long to ecosystem-level data
+# Add lat/long to ecosystem-level data
 ydata_eco_comb <- ydata_eco |>
   rownames_to_column(var = 'id') |>
   left_join(xdata_ind, by = 'id') |>
   select(c(Prairie, Savanna, Forest, lat, long))
 
-
 ## Plot
 
-#Plot Ecosystem state
+# Plot ecosystem state
 ydata_eco_comb |>
   pivot_longer(Prairie:Forest, names_to = 'Ecosystem', values_to = 'Presence') |>
   filter(Presence == 1) |>
@@ -60,21 +59,21 @@ ydata_eco_comb |>
   labs(color = 'Ecosystem State') +
   guides(color = guide_legend(override.aes = list(shape = 16, size = 7))) +
   geom_polygon(data = states, aes(x = long, y = lat, group = group), color = 'black', fill = NA) +
-  coord_fixed() +
   theme_void() +
+  coord_fixed() +
   theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5),
         legend.title = element_text(size = 12),
         legend.text = element_text(size = 12)) +
   ggtitle('Ecosystem distributions')
 
-#Plot Taxa Class
+# Plot taxon class
 pal <- c('#bb5566',
          '#ddaa34', '#ecd08f',
          '#002a53', '#004488', '#4c7cac', '#8aa9c8', '#c2d2e2', '#dee7f0',
          '#005f5f', '#008b8b', '#38a5a5', '#63b9b9', '#8ecdcd', '#c1e4e4')
 
 ydata_all_comb |>
-  pivot_longer(No.tree:Other.hardwood, names_to = 'Taxon', values_to = 'Presence') |>
+  pivot_longer(Elm:Other.hardwood, names_to = 'Taxon', values_to = 'Presence') |>
   filter(Presence == 1) |>
   mutate(Taxon = if_else(Taxon == 'No.tree', 'No tree', Taxon),
          Taxon = if_else(Taxon == 'Black.gum.sweet.gum', 'Black gum/sweet gum', Taxon),
@@ -88,20 +87,20 @@ ydata_all_comb |>
                                               'Ash', 'Basswood', 'Beech',
                                               'Black gum/sweet gum', 'Dogwood',
                                               'Elm', 'Ironwood', 'Maple', 'Other conifer',
-                                              'Other hardwood', 'Poplar/tulip poplar', 
+                                              'Other hardwood', 'Poplar/tulip poplar',
                                               'Walnut')) +
   geom_polygon(data = states, aes(x = long, y = lat, group = group), color = 'black', fill = NA) +
-  coord_fixed() +
   theme_void() +
+  coord_fixed() +
   theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5),
         legend.title = element_text(size = 12),
         legend.text = element_text(size = 12)) +
   ggtitle('Taxon distributions')
 
-## Summary Figures for X Data
+## Summary figures for x data
 
-# Plot Slope
-slope <- xdata |>
+# Plot slope
+slope <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = Slope), shape = '.') +
   scale_color_gradient(low = 'White', high = 'black', 'Slope (°)') +
@@ -114,8 +113,8 @@ slope <- xdata |>
   ggtitle('Slope')
 slope
 
-# Plot Aspect
-aspect <- xdata |>
+# Plot aspect
+aspect <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = direction), shape = '.') +
   labs(color = 'Aspect Direction') +
@@ -131,7 +130,7 @@ aspect <- xdata |>
 aspect
 
 # Plot SWI
-swi <- xdata |>
+swi <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = mean.SWI), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1, str_wrap('SAGA Wetness Index', width = 10)) +
@@ -144,10 +143,11 @@ swi <- xdata |>
   ggtitle('SAGA Wetness Index')
 swi
 
-plot_grid(slope, aspect, swi, nrow = 2, labels = c("A", "B", "C"))
+plot_grid(slope, aspect, swi, nrow = 2, labels = c('A', 'B', 'C'),
+          rel_widths = c(0.46, 0.54))
 
 # Plot CAC
-CAC <- xdata |>
+CAC <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = CAC), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1) +
@@ -162,7 +162,7 @@ CAC <- xdata |>
 CAC
 
 # Plot CEC
-CEC <- xdata |>
+CEC <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = CEC), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1, str_wrap('Cation exchange capacity (meq/100g)', width = 14)) +
@@ -176,7 +176,7 @@ CEC <- xdata |>
 CEC
 
 # Plot %CLA
-cla <- xdata |>
+cla <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = CLA), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1, '% clay', limits = c(0, 100)) +
@@ -190,7 +190,7 @@ cla <- xdata |>
 cla
 
 # Plot %SAN
-san <- xdata |>
+san <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = SAN), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1, '% sand', limits = c(0, 100)) +
@@ -204,7 +204,7 @@ san <- xdata |>
 san
 
 # Plot AWC
-AWC <- xdata |>
+AWC <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = WAT), shape = '.') +
   scale_color_viridis(option = 'viridis', direction = -1, str_wrap('Available water content (cm/cm)', width = 14)) +
@@ -217,8 +217,8 @@ AWC <- xdata |>
   ggtitle('Available water content')
 AWC
 
-# Plot the presence of Hydric Soils
-hydric <- xdata |>
+# Plot the presence of hydric soils
+hydric <- xdata_oos |>
   filter(Hydric == 'Yes') |>
   ggplot() +
   geom_point(aes(x = long, y = lat), shape = '.', color = 'black') +
@@ -232,8 +232,8 @@ hydric <- xdata |>
   ggtitle('Presence of hydric soils')
 hydric
 
-# Plot the presence of Floodplains
-flood <- xdata |>
+# Plot the presence of floodplains
+flood <- xdata_oos |>
   filter(Floodplain == 'Yes') |>
   ggplot() +
   geom_point(aes(x = long, y = lat), shape = '.') +
@@ -247,38 +247,32 @@ flood <- xdata |>
   ggtitle('Presence of floodplain')
 flood
 
-plot_grid(CAC, CEC, AWC, nrow = 2, labels = c("A", "B", "C"))
-plot_grid(cla, san, nrow = 1, labels = c("A", "B"))
-plot_grid(hydric, flood, nrow = 1, labels = c("A", "B"))
+plot_grid(CAC, CEC, AWC, nrow = 2, labels = c('A', 'B', 'C'))
+plot_grid(cla, san, nrow = 1, labels = c('A', 'B'))
+plot_grid(hydric, flood, nrow = 1, labels = c('A', 'B'))
 
-# Plot Mean Annual Precipitation
-precip <- xdata |>
+# Plot mean annual precipitation
+precip <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = totalPPT), shape = '.') +
   scale_color_distiller(palette = 'Blues', direction = 1, str_wrap('Mean annual precipitation 1895 - 1925 (mm)', width = 14)) +
   geom_polygon(data = states, aes(x = long, y = lat, group = group), color = 'black', fill = NA) +
   theme_void() +
-  coord_fixed() +
-  theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5),
+  theme(strip.text = element_text(size = 12),
         legend.title = element_text(size = 12),
-        legend.text = element_text(size = 12)) +
-  ggtitle('Precipitation')
+        legend.text = element_text(size = 12))
 precip
 
-# Plot Mean Annual Temperature
-temp <- xdata |>
+# Plot mean annual temperature
+temp <- xdata_oos |>
   ggplot() +
   geom_point(aes(x = long, y = lat, color = MeanTEMP), shape = '.') +
   scale_color_viridis(option = 'magma', str_wrap('Mean annual temperature 1895 - 1925 (°C)', width = 14)) +
   geom_polygon(data = states, aes(x = long, y = lat, group = group), color = 'black', fill = NA) +
   theme_void() +
-  coord_fixed() +
-  theme(plot.title = element_text(size = 16, face = 'bold', hjust = 0.5),
+  theme(strip.text = element_text(size = 12),
         legend.title = element_text(size = 12),
-        legend.text = element_text(size = 12)) +
-  ggtitle('Temperature')
+        legend.text = element_text(size = 12))
 temp
 
-plot_grid(precip, temp, nrow = 1, labels = c("A", "B"))
-
-
+plot_grid(precip, temp, nrow = 1, labels = c('A', 'B'))
